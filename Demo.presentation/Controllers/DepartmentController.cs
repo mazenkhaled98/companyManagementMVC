f2e780a2f7924b5627c3ad6a5f4ad5e44b6fb994
mazenkhaled98 <mazenkhaled8@gmail.com>
2025-09-17 06:42:30 +0300

    video2

--- Demo.presentation/Controllers/DepartmentController.cs
+++ Demo.presentation/Controllers/DepartmentController.cs
@@ -1,15 +1,69 @@
 ﻿
+using Demo.BusinessLogic.DTOS;
 using Demo.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Demo.presentation.Controllers
 {
-    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    public class DepartmentController(IDepartmentService _departmentService , IWebHostEnvironment _env , ILogger<DepartmentController> _logger) : Controller
     {
+        #region Index
         public IActionResult Index()
         {
-            var departments =_departmentService.GetAllDepartments();
+            var departments = _departmentService.GetAllDepartments();
             return View(departments);
         }
+
+        #endregion
+
+
+        #region Create
+        //return view form
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid)//server side validation
+            {
+                try { 
+                   int result= _departmentService.AddDepartment(departmentDto);
+                     if (result > 0)
+                     {
+                          
+                          return RedirectToAction(nameof(Index));
+                     }
+                     else
+                     {
+                          ModelState.AddModelError(string.Empty, "Failed to create department");
+                          
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    if(_env.IsDevelopment())
+                    { 
+                        
+                        _logger.LogError(ex, "Error occurred while creating department");
+                      
+                    }
+                    else
+                    {
+                                               _logger.LogError(ex, "Error occurred while creating department");
+                        return RedirectToAction("ErrorView");
+                    }
+                }
+            }
+            return View(departmentDto);
+
+
+        }
+        #endregion
     }
 }
