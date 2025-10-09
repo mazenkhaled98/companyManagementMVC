809dd586a1185cbcd2cc7a7a1e8239d08c126c95
mazenkhaled98 <mazenkhaled8@gmail.com>
2025-10-09 15:40:10 +0300

    video6

--- Demo.presentation/Controllers/UserController.cs
+++ Demo.presentation/Controllers/UserController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Demo.presentation.Controllers
 {
     [Authorize]
-    public class UserController(UserManager<ApplicationUser> _userManager) : Controller
+    public class UserController(UserManager<ApplicationUser> _userManager , IWebHostEnvironment _env) : Controller
     {
         //service ==> usermanager
         //index ,details ,edit ,delete ,[create user register]
@@ -38,6 +38,97 @@ namespace Demo.presentation.Controllers
 
             return View(users);
         }
+        #endregion
+
+        #region Details
+
+        [HttpGet]
+        public IActionResult Details(string? id)
+        {
+            if (id is null) return BadRequest();
+            var user = _userManager.FindByIdAsync(id).Result;
+            if (user is null) return NotFound();
+            var userViewModel = new UserViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Roles = _userManager.GetRolesAsync(user).Result
+            };
+            return View(userViewModel);
+        }
+
+        #endregion
+     
+        #region Edit
+
+        public IActionResult Edit(string? id)
+        {
+            if (id is null) return BadRequest();
+            var user = _userManager.FindByIdAsync(id).Result;
+            if (user is null) return NotFound();
+            var userViewModel = new UserViewModel()
+            {
+                Id = user.Id,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Roles = _userManager.GetRolesAsync(user).Result
+            };
+            return View(userViewModel);
+        }
+        [HttpPost]
+        public IActionResult Edit(UserViewModel userViewModel, string id)
+        {
+            if (!ModelState.IsValid) return View(userViewModel);
+            if (userViewModel.Id != id) return BadRequest();
+            string message = string.Empty;
+            try
+            {
+                var user = _userManager.FindByIdAsync(id).Result;
+                if (user is null) return NotFound();
+                user.FirstName = userViewModel.FirstName;
+                user.LastName = userViewModel.LastName;
+                user.Email = userViewModel.Email;
+                var result = _userManager.UpdateAsync(user).Result;
+                if (result.Succeeded) return RedirectToAction(nameof(Index));
+                else
+                    message = "User can't be updated!";
+            }
+            catch (Exception ex)
+            {
+                message = _env.IsDevelopment() ? ex.Message : "Some error occurred!";
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return View(userViewModel);
+        }
+
+        #endregion
+
+        #region Delete
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            var user = _userManager.FindByIdAsync(id).Result;
+            if (user is null) return NotFound();
+            string message = string.Empty;
+            try
+            {
+                var result = _userManager.DeleteAsync(user).Result;
+                if (result.Succeeded) return RedirectToAction(nameof(Index));
+                else
+                    message = "User can't be deleted!";
+            }
+            catch (Exception ex)
+            {
+                message = _env.IsDevelopment() ? ex.Message : "Some error occurred!";
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return RedirectToAction(nameof(Index));
+        }
+
         #endregion
     }
 }
